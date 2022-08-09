cdffe211ac6870fcd093da87eee650b61dc62d8d
Khrystyna <kr.bugaienko@gmail.com>
2022-08-09 18:16:15 +0200

    Create ourwork section
    * Add two svg images
    * Add slider using Slick
    * Styled dots under the slider
    * Add Slick js, css files to the project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 $(function(){
-
+  $('.ourwork__slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-left.svg"></img></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-right.svg"></img></button>',
+  });
 });
