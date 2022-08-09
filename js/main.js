8b6e5fb13e45b6ada92624c3fafd6b347a5a73ce
Khrystyna <kr.bugaienko@gmail.com>
2022-08-09 19:30:32 +0200

    Create advantages section
    * Add styles
    * Add background image for video
    * Add clickable video using magnific-popup libriary

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ $(function(){
     prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-left.svg"></img></button>',
     nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-right.svg"></img></button>',
   });
+  $('.video__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
 });
