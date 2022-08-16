4f70cce774ff183da15e43b3640461433fb2136f
Khrystyna <kr.bugaienko@gmail.com>
2022-08-16 22:19:32 +0200

    Change form relative to absolute path on the site

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(function(){
   $('.ourwork__slider').slick({
     dots: true,
-    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="images/arrow-left.svg"></img></button>',
-    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="images/arrow-right.svg"></img></button>',
+    prevArrow: '<button type="button" class="slick-prev slick-btn"><img src="/images/arrow-left.svg"></img></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"><img src="/images/arrow-right.svg"></img></button>',
   });
   $('.video__link').magnificPopup({
 		disableOn: 700,
